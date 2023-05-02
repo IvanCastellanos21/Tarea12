5801f04d7d5fd8129778d97c4baacf15c207e32e
IvanCastellanos21 <124224052+IvanCastellanos21@users.noreply.github.com>
2023-05-01 22:23:27 -0600

    Add files via upload

---
+++ Tarea12/src/Main.java
@@ -0,0 +1,7 @@
+import iu.principal;
+
+public class Main {
+    public static void main(String[] args) {
+        principal ventana = new principal("Hola mundo AWT");
+    }
+}
---
+++ Tarea12/src/iu/principal.java
@@ -0,0 +1,43 @@
+package iu;
+import iu.eventos.eventosRaton;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class principal extends Frame{
+    private Label lblNombre;
+    private TextField txtNombre;
+    private Button btnBoton1;
+
+    public principal(String title) throws HeadlessException {
+        super(title);
+        this.setSize(800,600);
+        this.setLayout(new FlowLayout());
+        lblNombre = new Label("");
+        this.add(lblNombre);
+        txtNombre = new TextField(30);
+        this.add(txtNombre);
+        btnBoton1 = new Button("Saludar");
+        //btnBoton1.addMouseListener(new eventosRaton());
+        btnBoton1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(txtNombre.getText());
+                JOptionPane.showMessageDialog(null, "Hola " + txtNombre.getText());
+            }
+        });
+        this.add(btnBoton1);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
+
+}
